a4dd8c86aaa8a3dae47d55ef3464c6d579bf9d69
上村侑 <kamiyuonep@yahoo.co.jp>
2019-12-20 20:34:53 +0000

    mynewsのリポジトリです

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -6,8 +6,20 @@
     <meta name = "viewport" content = "width=device-width, initial-scale=1">
     
     <title>MyNews</title>
+    
   </head>
   <body>
     <h1>Myニュース作成画面2</h1>
+    @extends('layouts.profile')
+    @section('title,プロフィール')
+    @section('content')
+    <div class = "container">
+      <div class = "row">
+        <div class = "col-md-8 mx-auto">
+          <h1>MYプロフィール</h1>
+        </div>
+      </div>
+    </div>
+    @endsection
   </body>
 </html>
