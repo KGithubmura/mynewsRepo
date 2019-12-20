9abd2f885c520a2dae59175e5d1bd7ec29a10f3f
上村侑 <kamiyuonep@yahoo.co.jp>
2019-12-20 20:01:44 +0000

    phpセクション8課題修正しました。確認よろしくお願いします。

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,23 +5,26 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class ProfilrController extends Controller
+class ProfileController extends Controller
 {
     //
-    public function  add()
+    public function add()
     {
-      return view('admin'.'profile'.'create');
+        return view('admin.profile.create');
     }
-    public function  create()
+
+    public function create()
     {
-      return redirect('admin/profile/create');
+        return redirect('admin/profile/create');
     }
-    public function  edit()
+
+    public function edit()
     {
-      return view('admin'.'profile'.'edit');
+        return view('admin.profile.edit');
     }
-    public function  update()
+
+    public function update()
     {
-      return redirect('admin/profile/edit');
+        return redirect('admin/profile/edit');
     }
 }
