8ee628c1e8f771404d80c3c01fd91786b9adc85d
上村侑 <kamiyuonep@yahoo.co.jp>
2019-12-24 05:04:08 +0000

    2019/12/24 pm14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -9,35 +9,35 @@ use App\News;
 class NewsController extends Controller
 {
     //
-public function add()
+    public function add()
     {
       return view ('admin.news.create');
     }
  
-public function create(Request $request)
+    public function create(Request $request)
     {
-      // Varidationを行う
-      $this->validate($request, News::$rules);
-      $news = new News;
-      $form = $request->all();
-
-      if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $news->image_path = basename($path);
-      } else {
-          $news->image_path = null;
-      }
-
-      unset($form['_token']);
-      unset($form['image']);
-
-      $news->fill($form);
-      $news->save();
-
-       return redirect('admin/news/create');
+        // Varidationを行う
+        $this->validate($request, News::$rules);
+        $news = new News;
+        $form = $request->all();
+        
+        if (isset($form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        
+        unset($form['_token']);
+        unset($form['image']);
+        
+        $news->fill($form);
+        $news->save();
+        
+        return redirect('admin/news/create');
     }
     
-public function index(Request $request)
+    public function index(Request $request)
     {  
       $cond_title = $request -> cond_title;
       if ($cond_title != ''){
@@ -48,37 +48,36 @@ public function index(Request $request)
       return view('admin.news.index',['posts' => $posts, 'cond_title' => $cond_title]);
     }
   
-public function edit(Request $request)
+    public function edit(Request $request)
     {
-      $news = News::find($request->id);
-      if (empty($news)) {
-        abort(404);    
-      }
-      return view('admin.news.edit', ['news_form' => $news]);
+        $news = News::find($request->id);
+        if (empty($news)) {
+            abort(404);    
+        }
+        return view('admin.news.edit', ['news_form' => $news]);
      }
 
 
-public function update(Request $request)
-     {
-       $this->validate($request, News::$rules);
-
-       $news = News::find($request->id);
-       $news_form = $request->all();
-       unset($news_form['_token']);
-
-       $news->fill($news_form)->save();
-
-       return redirect('admin/news');
-      }
+    public function update(Request $request)
+    {
+        $this->validate($request, News::$rules);
+        
+        $news = News::find($request->id);
+        $news_form = $request->all();
+        unset($news_form['_token']);
+        
+        $news->fill($news_form)->save();
+        
+        return redirect('admin/news');
+    }
       
 
-public function delete(Request $request) 
-      {
+    public function delete(Request $request) 
+    {
         $news = News::find($request->id);
         $news->delete();
         return redirect('admin/news/');
-      }
+    }
 
-  
 }
 
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -50,9 +50,11 @@
                             </div>
                         </div>
                     </div>
-                    <div class="form-group row"> 
-                        <div>
-                             <a href="{{ action('Admin\NewsController@edit', ['id' => $news->id]) }}">編集</a>
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                            {{ csrf_field() }}
+                            <input type="submit" class="btn btn-primary" value="更新">
                         </div>
                     </div>
                 </form>
